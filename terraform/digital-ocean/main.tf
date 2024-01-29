30f721fa2fa07b1c12f9bb2e6680110a6bd472db
daniel <daniel.pacheco@outlook.com>
2024-01-29 09:01:18 -0300

    fixing tf

--- terraform/digital-ocean/main.tf
+++ terraform/digital-ocean/main.tf
@@ -1,10 +1,10 @@
 resource "digitalocean_kubernetes_cluster" "spg-cluster" {
   name    = "spg-cluster"
   region  = "nyc1"
-  version = "1.22.8-do.1"
+  version = "1.27.9-do.0"
   node_pool {
     name       = "spg-pool"
     size       = "s-2vcpu-2gb"
     node_count = var.node_count
   }
-
+}
