86982838fe96c7b08536d4dbbf48232bfe389cf9
daniel <daniel.pacheco@outlook.com>
2024-02-03 18:30:56 -0300

    debuggin test

--- tests/tracing/tracingTest01.js
+++ tests/tracing/tracingTest01.js
@@ -2,10 +2,9 @@ import {sleep} from 'k6';
 import tracing from 'k6/x/tracing';
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
-
 export const options = {
     vus: 1,
-    duration: "2m",
+    duration: "20m",
 };
 
 const endpoint = "tempo.tempo.svc.cluster.local:4318"
@@ -15,34 +14,59 @@ const client = new tracing.Client({
     insecure: true,
 });
 
+const traceDefaults = {
+    attributeSemantics: tracing.SEMANTICS_HTTP,
+    attributes: {"one": "three"},
+    randomAttributes: {count: 2, cardinality: 5}
+}
+
+const traceTemplates = [
+    {
+        defaults: traceDefaults,
+        spans: [
+            {service: "shop-backend", name: "list-articles", duration: {min: 200, max: 900}},
+            {service: "shop-backend", name: "authenticate", duration: {min: 50, max: 100}},
+            {service: "auth-service", name: "authenticate"},
+            {service: "shop-backend", name: "fetch-articles", parentIdx: 0},
+            {service: "article-service", name: "list-articles"},
+            {service: "article-service", name: "select-articles", attributeSemantics: tracing.SEMANTICS_DB},
+            {service: "postgres", name: "query-articles", attributeSemantics: tracing.SEMANTICS_DB, randomAttributes: {count: 5}},
+        ]
+    },
+    {
+        defaults: {
+            attributes: {"numbers": ["one", "two", "three"]},
+            attributeSemantics: tracing.SEMANTICS_HTTP,
+        },
+        spans: [
+            {service: "shop-backend", name: "article-to-cart", duration: {min: 400, max: 1200}},
+            {service: "shop-backend", name: "authenticate", duration: {min: 70, max: 200}},
+            {service: "auth-service", name: "authenticate"},
+            {service: "shop-backend", name: "get-article", parentIdx: 0},
+            {service: "article-service", name: "get-article"},
+            {service: "article-service", name: "select-articles", attributeSemantics: tracing.SEMANTICS_DB},
+            {service: "postgres", name: "query-articles", attributeSemantics: tracing.SEMANTICS_DB, randomAttributes: {count: 2}},
+            {service: "shop-backend", name: "place-articles", parentIdx: 0},
+            {service: "cart-service", name: "place-articles", attributes: {"article.count": 1, "http.status_code": 201}},
+            {service: "cart-service", name: "persist-cart"}
+        ]
+    },
+    {
+        defaults: traceDefaults,
+        spans: [
+            {service: "shop-backend", attributes: {"http.status_code": 403}},
+            {service: "shop-backend", name: "authenticate", attributes: {"http.request.header.accept": ["application/json"]}},
+            {service: "auth-service", name: "authenticate", attributes: {"http.status_code": 403}},
+        ]
+    },
+]
 
 export default function () {
-    let pushSizeTraces = randomIntBetween(2,3);
-    let pushSizeSpans = 0;
-    let t = [];
-    for (let i = 0; i < pushSizeTraces; i++) {
-        let c = randomIntBetween(5,10)
-        pushSizeSpans += c;
+    const templateIndex = randomIntBetween(0, traceTemplates.length-1)
+    const gen = new tracing.TemplatedGenerator(traceTemplates[templateIndex])
+    client.push(gen.traces())
 
-        t.push({
-            random_service_name: false,
-            spans: {
-                count: c,
-                size: randomIntBetween(300,1000),
-                random_name: true,
-                fixed_attrs: {
-                    "test": "test",
-                },
-            }
-        });
-    }
-
-    let gen = new tracing.ParameterizedGenerator(t)
-    let traces = gen.traces()
-    client.push(traces);
-
-    console.log(`Pushed ${pushSizeSpans} spans from ${pushSizeTraces} different traces. Here is a random traceID: ${t[Math.floor(Math.random() * t.length)].id}`);
-    sleep(15);
+    sleep(randomIntBetween(1, 5));
 }
 
 export function teardown() {
