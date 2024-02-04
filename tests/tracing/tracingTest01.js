ee7d786187bc4d8e1573c716bbd471c18e7c8907
daniel <daniel.pacheco@outlook.com>
2024-02-04 13:34:03 -0300

    testing jaeger otlp port in k6 test

--- tests/tracing/tracingTest01.js
+++ tests/tracing/tracingTest01.js
@@ -8,8 +8,8 @@ export const options = {
 };
 
 //const endpoint = "tempo.tempo.svc.cluster.local:4317"
-//const endpoint = "jaeger-collector.jaeger.svc.cluster.local:14250"
-const endpoint = "otel-collector-opentelemetry-collector.jaeger.svc.cluster.local:4317"
+const endpoint = "jaeger-collector.jaeger.svc.cluster.local:14500"
+//const endpoint = "otel-collector-opentelemetry-collector.jaeger.svc.cluster.local:4317"
 const client = new tracing.Client({
     endpoint,
     exporter: tracing.EXPORTER_OTLP,
