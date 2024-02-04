5a26641fc78cd71482be0ce0174167d172bc2aed
daniel <daniel.pacheco@outlook.com>
2024-02-04 10:37:11 -0300

    forwading k6 test traces to otel collector before jaeger

--- tests/tracing/tracingTest01.js
+++ tests/tracing/tracingTest01.js
@@ -8,10 +8,11 @@ export const options = {
 };
 
 //const endpoint = "tempo.tempo.svc.cluster.local:4317"
-const endpoint = "jaeger-collector.jaeger.svc.cluster.local:14250"
+//const endpoint = "jaeger-collector.jaeger.svc.cluster.local:14250"
+const endpoint = "otel-collector-opentelemetry-collector.jaeger.svc.cluster.local:4317"
 const client = new tracing.Client({
     endpoint,
-    exporter: "jaeger",
+    exporter: "otel",
     insecure: true,
 });
 
