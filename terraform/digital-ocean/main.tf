55d7d11697f059eedcd57852e2ace2a33adb1361
Daniel <daniel.azepac@outlook.com>
2024-02-14 23:42:15 -0300

    Adjusting do terraform

--- terraform/digital-ocean/main.tf
+++ terraform/digital-ocean/main.tf
@@ -1,13 +1,13 @@
 resource "digitalocean_kubernetes_cluster" "spg-cluster" {
   name    = "spg-cluster"
   region  = "nyc1"
-  version = "1.27.9-do.0"
+  version = "1.27.10-do.0"
   ha = false
   auto_upgrade = false
   destroy_all_associated_resources = true
   node_pool {
     name       = "spg-pool"
-    size       = "s-2vcpu-2gb"
+    size       = "s-4vcpu-8gb"
     node_count = var.node_count
     auto_scale = false
   }
